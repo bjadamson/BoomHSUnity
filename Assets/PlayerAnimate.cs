908bdd1a91e6d2e12d6df8bb71106cc2dfba281a
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-01-16 22:29:25 -0800

    Initial commit

---
+++ Assets/PlayerAnimate.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimate : MonoBehaviour {
+
+	[SerializeField]
+	private Animator animator;
+
+	[SerializeField]
+	private float directionDampTime = 0.25f;
+
+	[SerializeField]
+	private ThirdPersonCamera tpCamera;
+
+	[SerializeField]
+	private float directionSpeed = 3.0f;
+
+	[SerializeField]
+	private float rotationDegreesPerSecond = 120f;
+
+	private float speed = 0.0f;
+	private float direction = 0.0f;
+	private float horizontal = 0.0f;
+	private float vertical = 0.0f;
+	private AnimatorStateInfo stateInfo;
+
+	private int m_locomotionId;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+
+		if (animator.layerCount >= 2) {
+			animator.SetLayerWeight (1, 1);
+		}
+
+		m_locomotionId = Animator.StringToHash ("Base Layer.Locomotion");
+	}
+
+	void Update () {
+		stateInfo = animator.GetCurrentAnimatorStateInfo (0);
+		horizontal = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
+		vertical = Input.GetAxis ("Vertical") * Time.deltaTime * 150.0f;
+
+		StickToWorldspace(transform, tpCamera.transform, ref direction, ref speed);
+		animator.SetFloat ("Speed", speed);
+		//animator.SetFloat ("Direction", direction, directionDampTime, Time.deltaTime);
+	}
+
+	void FixedUpdate() {
+		if (IsInLocomotion () && ((direction >= 0 && horizontal >= 0) || (direction < 0 && horizontal < 0))) {
+			Vector3 maxLerp = new Vector3 (0f, rotationDegreesPerSecond * (horizontal < 0f ? -1f : 1f), 0f);
+			Vector3 rotationAmount = Vector3.Lerp (Vector3.zero, maxLerp, Mathf.Abs (horizontal));
+			Quaternion deltaRotation = Quaternion.Euler(rotationAmount * Time.deltaTime);
+			transform.rotation = transform.rotation * deltaRotation;
+		}
+	}
+
+	private void StickToWorldspace(Transform root, Transform camera, ref float directionOut, ref float speedOut)
+	{
+		Vector3 rootDirection = root.forward;
+		Vector3 stickDirection = new Vector3 (horizontal, 0, vertical);
+
+		speedOut = stickDirection.sqrMagnitude;
+
+		// camera rotation
+		Vector3 cameraDirection = camera.forward;
+		cameraDirection.y = 0;
+		Quaternion shift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
+
+		// stick to worldspace
+		Vector3 moveDirection = shift * stickDirection;
+		Vector3 axisSign = Vector3.Cross (moveDirection, rootDirection);
+
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), moveDirection, Color.green);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), axisSign, Color.red);
+		Debug.DrawRay (new Vector3 (root.position.x, root.position.y + 2f, root.position.z), rootDirection, Color.magenta);
+
+		float multiplier = axisSign.y >= 0 ? -1f : 1f;
+		float angleRootToMove = Vector3.Angle (rootDirection, moveDirection) * multiplier;
+		angleRootToMove /= 180.0f;
+		directionOut = angleRootToMove * directionSpeed;
+	}
+
+	private bool IsInLocomotion() {
+		return stateInfo.nameHash == m_locomotionId;
+	}
+}
---
+++ Assets/ThirdPersonCamera.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour {
+	[SerializeField]
+	private Vector3 distanceAway = new Vector3 (0, 0, 2);
+
+	[SerializeField]
+	private float smooth;
+
+	[SerializeField]
+	private Transform followTransform;
+
+	[SerializeField]
+	private float cameraSmoothDampTime = 0.1f;
+
+	[SerializeField]
+	float rotationSpeed = 5;
+
+	[SerializeField]
+	float maxAngle = 30.0f;
+
+	private Vector3 cameraSmoothVelocity = Vector3.zero;
+	private bool freeLookMode = false;
+	private Vector3 prevPosition = new Vector3();
+	private Quaternion prevRotation = new Quaternion();
+
+	Vector2 prevMousePosition;
+
+	void Start() {
+		prevMousePosition = Input.mousePosition;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Mouse3)) {
+			freeLookMode = true;
+			prevPosition = transform.position;
+			prevRotation = transform.rotation;
+			Debug.Log ("");
+		} else if (Input.GetKeyUp(KeyCode.Mouse3)) {
+			freeLookMode = false;
+			transform.position = prevPosition;
+			transform.rotation = prevRotation;
+			Debug.Log ("");
+		}
+	}
+
+	void LateUpdate() {
+		Vector3 playerOffset = followTransform.position;
+
+		Vector3 lookDirection = playerOffset - transform.position;
+		lookDirection.y = 0;
+		lookDirection.Normalize ();
+		Vector3 targetPosition = playerOffset + (distanceAway.x * Vector3.right) + (followTransform.up * distanceAway.y) - (lookDirection * distanceAway.z);
+
+		if (freeLookMode) {
+			snapToPosition (targetPosition);
+			float horizontal = Input.GetAxis ("Mouse X") * rotationSpeed;
+			float currentlyRotated = Quaternion.Angle(transform.rotation, Quaternion.AngleAxis(0, Vector3.up));
+
+			transform.RotateAround (transform.position, Vector3.up, horizontal);
+
+			float cr2 = Quaternion.Angle(transform.rotation, Quaternion.AngleAxis(0, Vector3.up));
+			if (cr2 > maxAngle) {
+				transform.RotateAround (transform.position, Vector3.up, -horizontal);
+			}
+		} else {
+			smoothToPosition (transform.position, targetPosition);
+			transform.LookAt (followTransform);
+		}
+		prevMousePosition = Input.mousePosition;
+	}
+
+	private void snapToPosition(Vector3 targetPos) {
+		transform.position = targetPos;
+	}
+
+	private void smoothToPosition(Vector3 fromPos, Vector3 toPos) {
+		transform.position = Vector3.SmoothDamp (fromPos, toPos, ref cameraSmoothVelocity, cameraSmoothDampTime);
+	}
+}
