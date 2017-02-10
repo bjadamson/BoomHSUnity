1c61c0bd991f0694f5a8eb4fecd3769cf23fd05f
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-02-10 10:00:05 -0800

    Added main menu scene

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+	[SerializeField] private GameObject mainMenu;
+	[SerializeField] private GameObject optionsMenu;
+
+	void Start() {
+		MainMenuScene ();
+	}
+
+	public void LoadScene(string name) {
+		SceneManager.LoadScene (name);
+	}
+
+	public void MainMenuScene() {
+		mainMenu.SetActive (true);
+		optionsMenu.SetActive (false);
+	}
+
+	public void OptionsScene() {
+		mainMenu.SetActive (false);
+		optionsMenu.SetActive (true);
+	}
+
+	public void SaveOptions() {
+		MainMenuScene ();
+	}
+
+	public void CancelOptions() {
+		MainMenuScene ();
+	}
+
+	public void ExitGame() {
+		Application.Quit ();
+	}
+}
