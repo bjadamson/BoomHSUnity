7cfdddc4c4f4503d2864e6bb1fdd656b7bde394a
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-02-12 14:48:04 -0800

    Move panel logic to separate script

---
+++ Assets/ChatPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChatPanel : MonoBehaviour {
+	private Text[] textFields = new Text[50];
+
+	void Start() {
+		//row = textFields.Length;
+		for (int i = 0; i < 50; ++i) {
+			textFields [i] = addNewChatEntry (string.Empty, i);
+		}
+	}
+
+	private Text addNewChatEntry (string value, int row) {
+		GameObject uiText = new GameObject ("Text" + row.ToString ());
+		uiText.transform.SetParent (this.transform);
+		uiText.AddComponent<RectTransform> ();
+		Text text = uiText.AddComponent<Text> ();
+		text.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+		text.fontSize = 13;
+		text.color = Color.white;
+		text.verticalOverflow = VerticalWrapMode.Overflow;
+		text.horizontalOverflow = HorizontalWrapMode.Wrap;
+		text.alignByGeometry = true;
+		text.text = value;
+		text.rectTransform.pivot = new Vector2 (0.5f, 1.0f);
+		text.rectTransform.localScale = Vector3.one;
+
+		return text;
+	}
+
+	public void replaceChatEntry(string value) {
+		for (int i = 0; i < textFields.Length - 1; ++i) {
+			textFields [i].text = textFields [i + 1].text;
+		}
+		textFields [49].text = value;
+	}
+}
--- Assets/Scripts/Chat/ChatInput.cs
+++ Assets/Scripts/Chat/ChatInput.cs
@@ -7,22 +7,16 @@ using UnityEngine.UI;
 public class ChatInput : MonoBehaviour {
 	// config, move out
 	[SerializeField] private bool forceChatScrollBarToBottomAfterSubmit = false;
-	private static readonly int NUMBER_ROWS = 50;
 
 	[SerializeField] private InputField inputBox;
-	[SerializeField] private GameObject content;
+	[SerializeField] private ChatPanel chatPanel;
 	[SerializeField] private Scrollbar scrollbar;
 
 	private GameObject previouslySelected;
 	private float scrollPos;
-	private Text[] textFields = new Text[NUMBER_ROWS];
-	private int row;
 
 	void Start() {
-		row = textFields.Length;
-		for (int i = 0; i < row; ++i) {
-			textFields [i] = addNewChatEntry (string.Empty, i);
-		}
+	//	row = textFields.Length;
 		inputBox.gameObject.SetActive (false); // Initially input-field is hidden.
 		scrollbar.value = 0.0f; // Initially scrolled all the way down.
 	}
@@ -55,36 +49,11 @@ public class ChatInput : MonoBehaviour {
 
 	private void normalMode() {
 		popSelected ();
-		replaceChatEntry (inputBox.text);
+		chatPanel.replaceChatEntry (inputBox.text);
 		resetScrollBarPosition ();
 
 		// lastly clear out input box field.
 		inputBox.text = string.Empty;
-	}
-
-	private void replaceChatEntry(string value) {
-		for (int i = 0; i < textFields.Length - 1; ++i) {
-			textFields [i].text = textFields [i + 1].text;
-		}
-		textFields [49].text = value;
-	}
-
-	private Text addNewChatEntry (string value, int row) {
-		GameObject uiText = new GameObject ("Text" + row.ToString ());
-		uiText.transform.SetParent (content.transform);
-		uiText.AddComponent<RectTransform> ();
-		Text text = uiText.AddComponent<Text> ();
-		text.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
-		text.fontSize = 13;
-		text.color = Color.white;
-		text.verticalOverflow = VerticalWrapMode.Overflow;
-		text.horizontalOverflow = HorizontalWrapMode.Wrap;
-		text.alignByGeometry = true;
-		text.text = value;
-		text.rectTransform.pivot = new Vector2 (0.5f, 1.0f);
-		text.rectTransform.localScale = Vector3.one;
-
-		return text;
 	}
 
 	void resetScrollBarPosition () {
