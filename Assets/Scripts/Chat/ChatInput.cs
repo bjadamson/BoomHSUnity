cccf16bb0bbc441bc1396c6221720f81382162eb
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-02-12 11:45:04 -0800

    Lots of improvements to the chat window.

--- Assets/Scripts/Chat/ChatInput.cs
+++ Assets/Scripts/Chat/ChatInput.cs
@@ -6,13 +6,18 @@ using UnityEngine.UI;
 
 public class ChatInput : MonoBehaviour {
 	[SerializeField] private InputField inputBox;
-	[SerializeField] private Text textArea;
+	[SerializeField] private GameObject content;
+	[SerializeField] private Scrollbar scrollbar;
 
 	private GameObject previouslySelected;
+	private float scrollPos;
+	private int row = 0;
+
+	// config, move out
+	[SerializeField] private bool forceChatScrollBarToBottomAfterSubmit = false;
 
 	void Start() {
 		inputBox.gameObject.SetActive (false); // Initially input-field is hidden.
-		//textArea.text = string.Empty;
 	}
 
 	void Update () {
@@ -32,15 +37,41 @@ public class ChatInput : MonoBehaviour {
 	private void readInputMode() {
 		previouslySelected = EventSystem.current.currentSelectedGameObject;
 		inputBox.Select ();
+		scrollPos = scrollbar.value;
 	}
 
 	private void normalMode() {
-		string combined = textArea.text + "\n" + inputBox.text;
-		if (combined.Length > 500) {
-			combined.Remove (0, 100);
-		}
-		textArea.text = combined;
-		inputBox.text = string.Empty;
 		EventSystem.current.SetSelectedGameObject (previouslySelected);
+		addNewChatEntry ();
+		resetScrollBarPosition ();
+
+		// lastly clear out input box field.
+		inputBox.text = string.Empty;
+	}
+
+	private void addNewChatEntry () {
+		GameObject uiText = new GameObject ("Text" + row.ToString ());
+		++row;
+		uiText.transform.SetParent (content.transform);
+		uiText.AddComponent<RectTransform> ();
+		Text text = uiText.AddComponent<Text> ();
+		text.font = Resources.GetBuiltinResource<Font> ("Arial.ttf");
+		text.fontSize = 13;
+		text.color = Color.white;
+		text.verticalOverflow = VerticalWrapMode.Overflow;
+		text.horizontalOverflow = HorizontalWrapMode.Wrap;
+		text.alignByGeometry = true;
+		text.text = inputBox.text;
+		text.rectTransform.pivot = new Vector2 (0.5f, 1.0f);
+		text.rectTransform.localScale = Vector3.one;
+	}
+
+	void resetScrollBarPosition ()
+	{
+		if (forceChatScrollBarToBottomAfterSubmit) {
+			scrollbar.value = 0.0f;
+		} else {
+			scrollbar.value = scrollPos;
+		}
 	}
 }
