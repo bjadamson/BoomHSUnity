00c63c0035021973f33cacf40dbac09b62bd48e1
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-02-12 13:04:36 -0800

    Fixed the way the window works

--- Assets/Scripts/Chat/ChatInput.cs
+++ Assets/Scripts/Chat/ChatInput.cs
@@ -5,19 +5,26 @@ using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class ChatInput : MonoBehaviour {
+	// config, move out
+	[SerializeField] private bool forceChatScrollBarToBottomAfterSubmit = false;
+	private static readonly int NUMBER_ROWS = 50;
+
 	[SerializeField] private InputField inputBox;
 	[SerializeField] private GameObject content;
 	[SerializeField] private Scrollbar scrollbar;
 
 	private GameObject previouslySelected;
 	private float scrollPos;
-	private int row = 0;
-
-	// config, move out
-	[SerializeField] private bool forceChatScrollBarToBottomAfterSubmit = false;
+	private Text[] textFields = new Text[NUMBER_ROWS];
+	private int row;
 
 	void Start() {
 		inputBox.gameObject.SetActive (false); // Initially input-field is hidden.
+
+		row = textFields.Length;
+		for (int i = 0; i < row; ++i) {
+			textFields [i] = addNewChatEntry (string.Empty, i);
+		}
 	}
 
 	void Update () {
@@ -34,7 +41,6 @@ public class ChatInput : MonoBehaviour {
 		}
 
 		if (Input.GetKeyDown (KeyCode.Escape) && wasActive) {
-			Debug.Log ("typed '" + inputBox.textComponent.text + "'");
 			popSelected ();
 			resetScrollBarPosition ();
 			inputBox.gameObject.SetActive (false);
@@ -49,16 +55,22 @@ public class ChatInput : MonoBehaviour {
 
 	private void normalMode() {
 		popSelected ();
-		addNewChatEntry ();
+		replaceChatEntry (inputBox.text);
 		resetScrollBarPosition ();
 
 		// lastly clear out input box field.
 		inputBox.text = string.Empty;
 	}
 
-	private void addNewChatEntry () {
+	private void replaceChatEntry(string value) {
+		for (int i = 0; i < textFields.Length - 1; ++i) {
+			textFields [i].text = textFields [i + 1].text;
+		}
+		textFields [49].text = value;
+	}
+
+	private Text addNewChatEntry (string value, int row) {
 		GameObject uiText = new GameObject ("Text" + row.ToString ());
-		++row;
 		uiText.transform.SetParent (content.transform);
 		uiText.AddComponent<RectTransform> ();
 		Text text = uiText.AddComponent<Text> ();
@@ -68,9 +80,11 @@ public class ChatInput : MonoBehaviour {
 		text.verticalOverflow = VerticalWrapMode.Overflow;
 		text.horizontalOverflow = HorizontalWrapMode.Wrap;
 		text.alignByGeometry = true;
-		text.text = inputBox.text;
+		text.text = value;
 		text.rectTransform.pivot = new Vector2 (0.5f, 1.0f);
 		text.rectTransform.localScale = Vector3.one;
+
+		return text;
 	}
 
 	void resetScrollBarPosition () {
