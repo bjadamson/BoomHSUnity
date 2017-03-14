63c3d43c9e73526e9d0b1d35dab9954259620bd6
Benjamin Adamson <benjamin.adamson@gmail.com>
2017-03-14 15:49:06 -0700

    command window work

--- Assets/Scripts/UI/CommandWindow/CommandDatabase.cs
+++ Assets/Scripts/UI/CommandWindow/CommandDatabase.cs
@@ -3,35 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace ui
+namespace ui.command_window
 {
-	namespace command_window
+	public class CommandDatabase
 	{
-		public class CommandDatabase
+		private IList<Command> commands = new List<Command>();
+		public bool showingAtleastOne = false;
+
+		public void add(Command cmd)
 		{
-			private IList<Command> commands = new List<Command> ();
-			public bool showingAtleastOne = false;
+			commands.Add(cmd);
+		}
 
-			public void add (Command cmd)
-			{
-				commands.Add (cmd);
-			}
+		public IList<Command> getEntries()
+		{
+			return commands;
+		}
 
-			public IList<Command> getEntries ()
+		public IList<Command> getEntriesMatchingPrefix(string prefix)
+		{
+			IList<Command> matching = new List<Command>();
+			foreach (Command cmd in commands)
 			{
-				return commands;
-			}
-
-			public IList<Command> getEntriesMatchingPrefix (string prefix)
-			{
-				IList<Command> matching = new List<Command> ();
-				foreach (Command cmd in commands) {
-					if (cmd.name ().StartsWith (prefix)) {
-						matching.Add (cmd);
-					}
+				if (cmd.name().StartsWith(prefix))
+				{
+					matching.Add(cmd);
 				}
-				return matching;
 			}
+			return matching;
 		}
 	}
 }
--- Assets/Scripts/UI/CommandWindow/ViewPanel.cs
+++ Assets/Scripts/UI/CommandWindow/ViewPanel.cs
@@ -3,102 +3,107 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-namespace ui
+namespace ui.command_window
 {
-	namespace command_window
+	public class ViewPanel : MonoBehaviour
 	{
-		public class ViewPanel : MonoBehaviour
+		private static readonly int fontSize = 13;
+
+		[SerializeField] private GameObject commandAnchor;
+		[SerializeField] private Text emptyResultText;
+
+		[SerializeField] private InputField input;
+		private CommandDatabase commandDatabase = new CommandDatabase();
+		private int row = 0;
+
+		void Start()
 		{
-			private static readonly int fontSize = 13;
+			addCommand("who", "Display who is in your area.");
+			addCommand("where", "Display your location.");
+			addCommand("ffs", "................. fuck sakes");
+			addCommand("animals", "are not food");
+			addCommand("food", "see plants");
+			addCommand("plants", "energy to do things and not die.");
+			addCommand("gun", "bOOm");
+			addCommand("help", "for success, I need more cowbell!");
+			addCommand("zzzz", "For FRSFSFSFSF sakes");
+			addCommand("daybreak", "daybreak suuuuuuuuuxxxs");
+			addCommand("01001", "0xDEADBEEF");
+			addCommand("pizza", "ordering pizza now..");
+			addCommand("rename", "Rename a chat channel, ie: '/rename channel group party'");
 
-			[SerializeField] private GameObject commandAnchor;
-			[SerializeField] private Text emptyResultText;
+			emptyResultText.text = string.Empty;
+		}
 
-			[SerializeField] private InputField input;
-			private CommandDatabase commandDatabase = new CommandDatabase ();
-			private int row = 0;
+		private void addCommand(string name, string description)
+		{
+			string guiText = "Command" + row;
+			row++;
 
-			void Start ()
+			commandDatabase.add(new Command(commandAnchor, guiText, name, description, fontSize));
+		}
+
+		void Update()
+		{
+			string userText = input.text.Trim();
+			if (userText == string.Empty)
 			{
-				addCommand ("who", "Display who is in your area.");
-				addCommand ("where", "Display your location.");
-				addCommand ("ffs", "................. fuck sakes");
-				addCommand ("efs", "xxxxxxxxxx fuck sakes");
-				addCommand ("def", "XXXXXXXXXX fuck sakes");
-				addCommand ("ffs", "For fuck sakes");
-				addCommand ("abc", "For fuck sakes");
-				addCommand ("ffs", "For fuck sakes");
-				addCommand ("zzzz", "For FRSFSFSFSF sakes");
-				addCommand ("ffs", "ZZZZZZZZZZ fuck sakes");
-				addCommand ("ser", "TTTTTTTTTTTTTTTT fuck sakes");
-				addCommand ("zcvzz", "66666666666 fuck sakes");
-				addCommand ("aaaa", "For fuck sakes");
-				addCommand ("rename channel", "Rename a chat channel, ie: '/rename channel group party'");
-
-				emptyResultText.text = string.Empty;
+				showEntriesMatchingPrefix("");
+				return;
 			}
 
-			private void addCommand (string name, string description)
+			if (userText.StartsWith("/"))
 			{
-				string guiText = "Command" + row;
-				row++;
-
-				commandDatabase.add (new Command (commandAnchor, guiText, name, description, fontSize));
+				userText = userText.Remove(0, 1);
 			}
 
-			void Update ()
+			IList<Command> matchingCommands = commandDatabase.getEntriesMatchingPrefix(userText);
+			showEntriesMatchingPrefix(userText);
+			hideAllEntries();
+			showEntriesMatchingPrefix(userText);
+
+			if (matchingCommands.Count == 0)
 			{
-				string userText = input.text.Trim ();
-				if (userText == string.Empty) {
-					return;
-				}
-
-				if (userText.StartsWith ("/")) {
-					userText = userText.Remove (0, 1);
-				}
-
-				IList<Command> matchingCommands = commandDatabase.getEntriesMatchingPrefix (userText);
-				showEntriesMatchingPrefix (userText);
-				hideAllEntries ();
-				showEntriesMatchingPrefix (userText);
-
-				if (matchingCommands.Count == 0) {
-					displayNoMatchingResult ();
-				} else {
-					hideNoMatchingResults ();
-				}
+				displayNoMatchingResult();
 			}
-
-			private void showEntriesMatchingPrefix (string prefix)
+			else
 			{
-				foreach (Command cmd in commandDatabase.getEntries()) {
-					if (cmd.name ().StartsWith (prefix)) {
-						cmd.show ();
-					}
+				hideNoMatchingResults();
+			}
+		}
+
+		private void showEntriesMatchingPrefix(string prefix)
+		{
+			foreach (Command cmd in commandDatabase.getEntries())
+			{
+				if (cmd.name().StartsWith(prefix))
+				{
+					cmd.show();
 				}
 			}
+		}
 
-			private void hideAllEntries ()
+		private void hideAllEntries()
+		{
+			foreach (Command cmd in commandDatabase.getEntries())
 			{
-				foreach (Command cmd in commandDatabase.getEntries()) {
-					cmd.hide ();
-				}
+				cmd.hide();
 			}
+		}
 
-			private void displayNoMatchingResult ()
-			{
-				showNoMatchingResults ();
-			}
+		private void displayNoMatchingResult()
+		{
+			showNoMatchingResults();
+		}
 
-			private void hideNoMatchingResults ()
-			{
-				emptyResultText.text = string.Empty;
-			}
+		private void hideNoMatchingResults()
+		{
+			emptyResultText.text = string.Empty;
+		}
 
-			private void showNoMatchingResults ()
-			{
-				emptyResultText.text = "No commands found beginning with '" + input.text + "'";
-			}
+		private void showNoMatchingResults()
+		{
+			emptyResultText.text = "No commands found beginning with '" + input.text + "'";
 		}
 	}
 }
